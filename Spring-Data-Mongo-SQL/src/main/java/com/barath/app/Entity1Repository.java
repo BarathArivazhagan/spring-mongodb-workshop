77d3f5706e2ef4b7fe31d33591caaf04cde6666a
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-01-07 18:21:08 +0530

    Add files via upload

---
+++ Spring-Data-Mongo-SQL/src/main/java/com/barath/app/Entity1Repository.java
@@ -0,0 +1,7 @@
+package com.barath.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Entity1Repository extends JpaRepository<Entity1, Long> {
+
+}
---
+++ Spring-Data-Mongo-SQL/src/main/java/com/barath/app/Entity2Repository.java
@@ -0,0 +1,7 @@
+package com.barath.app;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Entity2Repository extends MongoRepository<Entity2, Long>{
+
+}
