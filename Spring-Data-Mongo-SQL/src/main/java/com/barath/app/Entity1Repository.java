d660b8a1b6423ba12c371397e28066c8c7f438ad
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-04 21:31:26 +0530

    delete mongodb -sql project

--- Spring-Data-Mongo-SQL/src/main/java/com/barath/app/Entity1Repository.java
+++
@@ -1,7 +0,0 @@
-package com.barath.app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Entity1Repository extends JpaRepository<Entity1, Long> {
-
-}
--- Spring-Data-Mongo-SQL/src/main/java/com/barath/app/Entity2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.barath.app;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Entity2Repository extends MongoRepository<Entity2, Long>{
-
-}
